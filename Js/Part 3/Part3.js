9671966e6b8fb3e4ad65be625a9f0f3601897fa0
Rahul Vishwakarma <vishwa.rahul638@gmail.com>
2025-04-16 10:59:27 +0530

    setting all js files to right position

---
+++ Js/Part 3/Part3.js
@@ -0,0 +1,14 @@
+// let arr = [7,9,0,-2];
+// let n = prompt('Enter the number of element do you want to print from first')
+// arr.slice(3);
+// console.log(arr)
+
+
+// Que 3 - Write a javaScript program to check whether the string is blank or not?
+
+let str = "";
+if (str.length == 0) {
+    console.log("String is Empty.")
+}else{
+    console.log("String is not Empty.")
+}
