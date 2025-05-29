4e2fe9669476cecee6b9898eb9837e58b6044c3c
Rahul Vishwakarma <vishwa.rahul638@gmail.com>
2025-05-29 17:44:43 +0530

    added the code of Todo list using REST.

--- Project/TodoList using REST/index.js
+++ Project/TodoList using REST/index.js
@@ -1,10 +1,12 @@
 const express = require("express")
+var methodOverride = require('method-override')
 const app = express();
 const { v4: uuidv4 } = require('uuid');
 const path = require("path")
 const port = 3000;
 let allTasks = require("./main")
 
+app.use(methodOverride('_method'))
 
 app.set("view engine","ejs")
 app.set("views",path.join(__dirname,"views"))
@@ -15,29 +17,39 @@ app.use(express.urlencoded({extended:true}))
 // app.use(express.json())
 
 app.get("/lists",(req,res) =>{
-    console.log("Tasks being sent to template:", allTasks);
     res.render("index.ejs",{allTasks})
 })
 
 app.post("/add",(req,res) =>{
     let {input} = req.body;
-    let newTask = {
-        // id: Date.now().toString(), // Generate unique ID
-        id:uuidv4(),
-        task: input
-    };
-    allTasks.push(newTask);
-    console.log("Updated tasks:", allTasks);
-    res.redirect("/lists");
+    if(input == ""){
+        res.redirect("/lists")
+    }else{
+        let newTask = {
+            // id: Date.now().toString(), // Generate unique ID
+            id:uuidv4(),
+            task: input
+        };
+        allTasks.push(newTask);
+        res.redirect("/lists");
+    }
 })
 app.patch("/lists/:id",(req,res) =>{
     let {id} = req.params
-    console.log(id)
-    // let task = allTasks.find((p) => id === p.id);
-    // res.render("edit.ejs", {task})
-    res.send("patch req acceped.")
+    let task = allTasks.find((p) => id === p.id);
+    res.render("edit.ejs", {task})
 })
-app.post("/submit",(req,res) =>{
+app.post("/lists/:id",(req,res) =>{
+    let {id} = req.params
+    let {input} = req.body
+    let task = allTasks.find((p) => id === p.id);
+    task.task = input
+    res.redirect("/lists")
+})
+
+app.delete("/lists/:id", (req,res) =>{
+    let {id} = req.params
+    allTasks = allTasks.filter((p) => id !== p.id)
     res.redirect("/lists")
 })
 
