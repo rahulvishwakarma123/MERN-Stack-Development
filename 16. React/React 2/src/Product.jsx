89f054e4f7041d71a28d9e52bc46144239de1ff4
Rahul Vishwakarma <vishwa.rahul638@gmail.com>
2025-07-21 18:26:47 +0530

    Added the code of react part 2.

--- 16. React/React 2/src/Product.jsx
+++ 16. React/React 2/src/Product.jsx
@@ -1,8 +1,13 @@
-function Product() {
+function Product({title, prise, featureArr, featureObj}) {
+    console.log(title, prise)
+    let list = featureArr.map((feature) => {<li>{feature}</li>})
     return (
         <>
-            <h3>This is Product title.</h3>
-            <p>This is Product description.</p>
+            <h3>This is {title}.</h3>
+            <p>The prise of the product is {prise}.</p>
+            <p>{featureArr}</p>
+            <p>{featureObj.a}</p>
+            <p>{list}</p>
         </>
     )
 }
--- 16. React/React 2/src/ProductTab.jsx
+++ 16. React/React 2/src/ProductTab.jsx
@@ -1,10 +1,16 @@
 import Product from "./Product"
 function ProductTab() {
+    let options1 = ['Hi-tech', 'durable', 'fast']
+    let options2 = {
+        a : "hi-tech",
+        b : "durable",
+        c : "fast"
+    }
     return (
         <>
-            <Product/>
-            <Product/>
-            <Product/>
+            <Product title="Laptop" prise = "60k" featureArr = {options1} featureObj = {options2}/>
+            <Product title = "Phone" prise = "20k" featureArr={options1}  featureObj = {options2}/>
+            <Product title = "Tablet" prise = "40k" featureArr = {options1} featureObj = {options2}/>
         </>
     )
 }
